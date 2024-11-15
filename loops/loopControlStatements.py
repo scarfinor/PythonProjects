6d241da783bdf0d7f4530687432adffc5a4f8a13
scarfinor <scarfinor@gmail.com>
2024-11-15 17:08:46 -0600

    added loopControlStatements.py (ways to change the result of a loops expected sequence) types.py (for loops, while loops, countdown timer) nestedLoops.py (put a loop inside a loop that executes after first loop completes all iterations)

--- loops/loopControlStatements.py
+++ loops/loopControlStatements.py
@@ -1,15 +1,30 @@
-from loops.types import newCode, newConcept
+from locale import locale_alias
+
+newCode = "--------------------"
 
 while True :
     name = input("Enter your name: ")
     if name != "" :
-        break
+       break
 
 print(newCode)
 
 phoneNumber = "123-456-7890"
+stop = " "
 
 for i in phoneNumber :
     if i == "-" :
         continue
-        print(i, end = "")
+    print(i, end="")
+print(stop)
+
+print(newCode)
+
+for i in range (1, 5) :
+    if i == 3 :
+        pass
+    else:
+        print(i, end="")
+print(stop)
+
+print("====================")
