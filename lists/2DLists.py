dafab0f7c027f91b99f953878fc8c95bf67f9f36
scarfinor <scarfinor@gmail.com>
2024-11-15 19:10:56 -0600

    added methods.py (some useful list methods) 2DLists.py (user inputs a meal and displays that meal)

---
+++ lists/2DLists.py
@@ -0,0 +1,16 @@
+newCode = "------------------------"
+food = input("enter food: ")
+drink = input("enter drink: ")
+desert = input("enter desert: ")
+dinner = [food, drink, desert]
+week = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
+print(dinner)
+print(newCode)
+
+mondaysMeal = [week[0],dinner]
+
+print(mondaysMeal)
+print(newCode)
+
+
---
+++ lists/methods.py
@@ -0,0 +1,33 @@
+newCode = "----------------------------------"
+newConcept = "==================================="
+meat = ["pork","beef","chicken","fish"]
+
+print(meat)
+print(newCode)
+
+print(meat[2])
+print(newCode)
+
+meat[3] = "human flesh"
+print(meat)
+print(newCode)
+
+meat.append("chicken")
+print (meat)
+print(newCode)
+
+meat.remove("chicken")
+print(meat)
+print(newCode)
+
+meat.remove("chicken")
+print(meat)
+print(newCode)
+
+meat.insert(0, "chicken")
+print(meat)
+print(newCode)
+
+meat.clear()
+print(meat)
+print(newCode)
